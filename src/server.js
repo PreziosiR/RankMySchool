9c5ba3ed961949b0106ee50567c37eb9b625eed1
RPR <romain.preziosi@lmba.fr>
2024-01-24 17:30:16 +0100

    wip

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import axios from 'axios';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/explore/v2.1/catalog/datasets/fr-en-ips_ecoles_v2/records', async (req, res) => {
+  try {
+    const response = await axios.get('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-ips_ecoles_v2/records');
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while trying to fetch data');
+  }
 });
 
 app.listen(port, () => {
