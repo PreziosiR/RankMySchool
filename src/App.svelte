4449246227c9d0e1e14c0e78334bf8ed7a99a676
RPR <romain.preziosi@lmba.fr>
2024-06-21 10:16:20 +0200

    basic api call + display school IPS

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,24 @@
 <script>
-  let data = [];
+  let schoolsDataset = { results: [] };
 
   async function fetchData() {
-    const response = await fetch('http://localhost:3000/api/explore/v2.1/catalog/datasets/fr-en-ips_ecoles_v2/records');
-    data = await response.json();
+    const response = await fetch('https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-en-ips_ecoles_v2/records?limit=20');
+    schoolsDataset = await response.json();
   }
-
+   // Créez une réaction qui se déclenche chaque fois que schoolsDataset est mis à jour 
+  $: results = schoolsDataset.results;
   fetchData();
 </script>
 
-{#each data as item (item.id)}
-  <p>{item.ips}</p>
-{/each}
+<h1>RankMySchool</h1>
+{console.log(results)}
+<ul>
+  {#each results as { ips }, i}
+    <li>
+        {i + 1}:  <p>{ips}</p>
+    </li>
+  {/each}
+</ul>
 
 <style>
 
